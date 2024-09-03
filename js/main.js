af4489baff384710c948704caf7a4ce289ccb2d4
Albey chowdhury <alveychow105@gmail.com>
2024-09-03 16:50:07 +0600

    fix git problem

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
     // Initiate the wowjs
     new WOW().init();
 
@@ -78,4 +67,3 @@
     });
     
 })(jQuery);
-
